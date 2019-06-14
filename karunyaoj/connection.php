2723f085f3efc24d68773867370c5cc87b483298
josethomas98 <josethomas16@karunya.edu.in>
2019-06-14 16:44:04 +0530

    added files

---
+++ karunyaoj/connection.php
@@ -0,0 +1,26 @@
+<?php
+
+$host = getenv("MYSQL_SERVICE_HOST");
+$port = getenv("MYSQL_SERVICE_PORT");
+$user = getenv("databaseuser");
+$pass = getenv("databasepassword");
+$db="karunya";
+/*
+//below has to be commented
+$con=mysqli_connect($host,$user,$pass,$db);
+
+if(!$con)
+{
+	print("Not Connected<br>".mysql_error());
+}
+else
+{
+
+}
+
+*/
+
+
+
+
+?>
